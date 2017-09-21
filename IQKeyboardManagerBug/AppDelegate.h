b6432b542c369db0116d8cf4f167f80386086f5f
hehuachaoKevin <847570232@qq.com>
2017-09-21 18:46:17 +0800

    Initial Commit

---
+++ IQKeyboardManagerBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IQKeyboardManagerBug
+//
+//  Created by KevinHe on 2017/9/21.
+//  Copyright © 2017年 KevinHehuachao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IQKeyboardManagerBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IQKeyboardManagerBug
+//
+//  Created by KevinHe on 2017/9/21.
+//  Copyright © 2017年 KevinHehuachao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
